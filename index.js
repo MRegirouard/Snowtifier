af4ebaa3d406d2df0668ac5ed0d88e52587dca2c
Ethan Girouard <mr.egirouard@gmail.com>
2021-12-02 21:03:03 -0500

    Write config options reading

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const confReader = require('@eta357/config-reader')
 const twitter = require('twitter')
 
+const configFile = './Options.json'
+const configOptions =
+{
+	'API Key': '',
+	'API Key Secret': '',
+	'Bearer Token': ''
+}
+var config
+
+confReader.readOptions(configFile, configOptions).then((options) =>
+{
+	console.info('[  OK  ] Successfully read config information.')
+	config = options
+}).catch((error) =>
+{
+	console.error(error)
+	process.exit(1)
+})
