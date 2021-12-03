9e526d773350ba90e8a9f9820476a0b6952df3e0
Ethan Girouard <mr.egirouard@gmail.com>
2021-12-03 13:17:49 -0500

    Write getExtraVal function

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ const configOptions =
 }
 var config
 
+/**
+ * Convert a school type string to an "extra val" for the snow day calculator.
+ * @param {string} schoolType The school type. Can be 'Public', 'Urban Public',
+ * 'Rural Public', 'Private', or 'Boarding'.
+ * @returns The "extra val" for snowdaycalculator.com
+ */
+function getExtraVal(schoolType)
+{
+	switch (schoolType.toLowerCase())
+	{
+		case 'urban public':
+			return 0.4
+		case 'rural public':
+		case 'private':
+			return -0.4
+		case 'boarding':
+			return 1
+		case 'public':
+		default:
+			return 0
+	}
+}
 confReader.readOptions(configFile, configOptions).then((options) =>
 {
 	console.info('[  OK  ] Successfully read config information.')
