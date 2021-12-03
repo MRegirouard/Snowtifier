93c6f699441c2c744f14f106ab2ac197506561d5
Ethan Girouard <mr.egirouard@gmail.com>
2021-12-03 13:18:19 -0500

    Write getPrediction function

--- index.js
+++ index.js
@@ -36,6 +36,51 @@ function getExtraVal(schoolType)
 			return 0
 	}
 }
+
+/**
+ * Gets the prediction for the desired date from the snow day calculator.
+ * @param {Date} date The date to get the prediction for.
+ * @param {string} zipCode The zip code to get the prediction for.
+ * @param {string} snowDayCount The number of snow days the school has had so far.
+ * @param {string} schoolType The school type. Can be 'Public', 'Urban Public',
+ * 'Rural Public', 'Private', or 'Boarding'.
+ * @returns A promise that resolves to the prediction for the desired date, or
+ * rejects with an error or if there is no prediction for the desired date.
+ */
+function getPrediction(date, zipCode, snowDayCount, schoolType)
+{
+	return new Promise((resolve, reject) =>
+	{
+		const extraVal = getExtraVal(schoolType) // The extra val for the school type
+
+		// Query the web page for the prediction
+		fetch('https://snowdaycalculator.com/prediction.php?zipcode=' + zipCode + '&snowdays=' + snowDayCount + '&extra=' + extraVal)
+			.then((response) => response.text())
+			.then((body) =>
+			{
+				// Find the date code from the given date
+				// Format: YYYYMMDD
+				const dateCode = date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice('-2') + ('0' + date.getDate()).slice('-2')
+
+				for (const line of body.split('\n')) // Loop through the lines
+				{
+					if (line.startsWith(' theChance[' + dateCode + '] = ')) // Find the desired line
+					{
+						var prediction = line.split(' ')[3]
+						prediction = prediction.slice(0, prediction.length - 1)
+						prediction = Math.min(prediction, 99) // Limit the prediction from 0 to 99%
+						prediction = Math.max(prediction, 0)
+						resolve(prediction) // Resolve the promise with the prediction
+						return
+					}
+				}
+
+				reject('No prediction found') // If we make it here, no prediction was found
+				return
+			})
+			.catch((error) => reject(error))
+	})
+}
 confReader.readOptions(configFile, configOptions).then((options) =>
 {
 	console.info('[  OK  ] Successfully read config information.')
